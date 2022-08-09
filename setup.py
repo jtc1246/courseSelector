84cf3865724234ede49b3b33dee0552ea908bae6
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2022-08-09 21:08:35 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='courseSelector',
-    version='1.0.8',
+    version='1.1.0',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/courseSelector',
     description='An automatic course selection program for SJTU-JI',
     packages=['courseSelector'],
-    install_requires=['urllib3'],
+    install_requires=['urllib3','myBasics','selenium==3.11.0','pillow','ddddocr','phantomjs-packages'],
     python_requires='>=3',
     platforms=["all"],
     license='GPL-2.0 License'
