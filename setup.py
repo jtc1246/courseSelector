3b8b211e78ac87924e9aea97c05f241f87635fc0
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2022-08-10 20:16:08 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='courseSelector',
-    version='1.1.1',
+    version='1.1.2',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/courseSelector',
